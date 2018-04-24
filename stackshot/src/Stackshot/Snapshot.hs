43c5811face4dfd911b9b895f2a1e44054b8cc18
David Baynard <davidbaynard@gmail.com>
2018-04-24 17:23:35 +0100

    Parse snapshot.yaml file

--- stackshot/src/Stackshot/Snapshot.hs
+++ stackshot/src/Stackshot/Snapshot.hs
@@ -6,5 +6,16 @@ module Stackshot.Snapshot
   ( module Stackshot.Snapshot
   ) where
 
-import "aeson" Data.Aeson.Types
-import "yaml" Data.Yaml hiding ((.=))
+import           "base"     Control.Monad
+import           "base"     Data.Bifunctor
+import           "yaml"     Data.Yaml hiding ((.=))
+import           "this"     Stackshot.Internal
+import qualified "this"     Stackshot.Snapshot.Auto as A
+import           "unliftio" UnliftIO (MonadUnliftIO, liftIO)
+
+type SnapshotYaml = A.TopLevel
+
+parseSnapshotFile' :: MonadUnliftIO m => FilePath -> m (Either Error SnapshotYaml)
+parseSnapshotFile'
+    = pure . parserError . first show
+  <=< readError . liftIO . decodeFileEither
