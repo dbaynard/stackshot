952222b925cb0dca3730018376c2a64b7d0518d7
David Baynard <davidbaynard@gmail.com>
2018-04-24 16:59:50 +0100

    Add Stackshot.Snapshot

---
+++ stackshot/src/Stackshot/Snapshot.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE
+    PackageImports
+  #-}
+
+module Stackshot.Snapshot
+  ( module Stackshot.Snapshot
+  ) where
+
+import "aeson" Data.Aeson.Types
+import "yaml" Data.Yaml hiding ((.=))
