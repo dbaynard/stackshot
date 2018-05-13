7742d46a874afa955f45832a454573c82eebcc63
David Baynard <davidbaynard@gmail.com>
2018-05-13 02:11:49 +0100

    Extract versions from github cabal files

--- stackshot/src/Stackshot/Git.hs
+++ stackshot/src/Stackshot/Git.hs
@@ -23,6 +23,11 @@ import           "base64-bytestring-type" Data.ByteString.Base64.Type
 import           "text"                   Data.Text (Text)
 import qualified "text"                   Data.Text as T
 import           "text"                   Data.Text.Encoding
+import           "Cabal"                  Distribution.Package (PackageName, pkgVersion)
+import           "Cabal"                  Distribution.PackageDescription
+import           "Cabal"                  Distribution.PackageDescription.Parse
+import           "Cabal"                  Distribution.Version (Version, mkVersion)
+import           "Cabal"                  Distribution.Verbosity (normal)
 import           "base"                   GHC.Generics
 import           "github"                 GitHub.Data hiding (Error)
 import qualified "github"                 GitHub.Endpoints.Repos.Contents as GC
@@ -51,10 +56,25 @@ exampleGithubRepo = RepoPackage
   , rCommit   = Just "develop"
   }
 
+githubVersion :: RepoPackage -> IO (Either Error Version)
+githubVersion rp = do
+  cbl <- first (ParserError . T.pack . show) <$> repoCabal rp
+  pure $ do
+    pkg <- decodeCabal =<< decodeGHContent =<< cbl
+    pure . pkgVersion . package . packageDescription $ pkg
+
 repoCabal :: RepoPackage -> IO (Either GC.Error Content)
 repoCabal RepoPackage{rSource = GitHub, ..} = do
   GC.contentsFor rOwner rRepo rFilepath rCommit
 
+decodeCabal :: Text -> Either Error GenericPackageDescription
+decodeCabal = parsing . parseGenericPackageDescription . T.unpack
+
 decodeGHContent :: Content -> Either Error Text
 decodeGHContent (ContentFile (ContentFileData{contentFileEncoding = "base64", ..})) =
-  first (ParserError . T.pack . show) . decodeUtf8' <=< first (ParserError . T.pack) . B64.decode . encodeUtf8 $ contentFileContent
+  first (ParserError . T.pack . show) . decodeUtf8' <=< first (ParserError . T.pack) . B64.decode . encodeUtf8 . T.filter (/= '\n') $ contentFileContent
+
+parsing :: ParseResult a -> Either Error a
+parsing (ParseOk [] a) = Right a
+parsing (ParseOk ws _) = Left . ParserError . T.pack . show $ ws
+parsing (ParseFailed e) = Left . ParserError . T.pack . show $ e
