2770ee2945434404942b147d358d33149fc7775a
David Baynard <davidbaynard@gmail.com>
2018-05-13 02:11:23 +0100

    Decode base64 files from github api request

--- stackshot/src/Stackshot/Git.hs
+++ stackshot/src/Stackshot/Git.hs
@@ -15,10 +15,18 @@ module Stackshot.Git
   ( module Stackshot.Git
   ) where
 
-import           "text"   Data.Text (Text)
-import           "base"   GHC.Generics
-import           "github" GitHub.Data
-import qualified "github" GitHub.Endpoints.Repos.Contents as GC
+import           "base"                   Control.Monad
+import           "base"                   Data.Bifunctor
+import           "bytestring"             Data.ByteString (ByteString)
+import qualified "base64-bytestring"      Data.ByteString.Base64 as B64
+import           "base64-bytestring-type" Data.ByteString.Base64.Type
+import           "text"                   Data.Text (Text)
+import qualified "text"                   Data.Text as T
+import           "text"                   Data.Text.Encoding
+import           "base"                   GHC.Generics
+import           "github"                 GitHub.Data hiding (Error)
+import qualified "github"                 GitHub.Endpoints.Repos.Contents as GC
+import           "this"                   Stackshot.Internal
 
 data RepoSource
   = GitHub
@@ -43,6 +51,10 @@ exampleGithubRepo = RepoPackage
   , rCommit   = Just "develop"
   }
 
-repoCabal :: RepoPackage -> IO (Either Error Content)
+repoCabal :: RepoPackage -> IO (Either GC.Error Content)
 repoCabal RepoPackage{rSource = GitHub, ..} = do
   GC.contentsFor rOwner rRepo rFilepath rCommit
+
+decodeGHContent :: Content -> Either Error Text
+decodeGHContent (ContentFile (ContentFileData{contentFileEncoding = "base64", ..})) =
+  first (ParserError . T.pack . show) . decodeUtf8' <=< first (ParserError . T.pack) . B64.decode . encodeUtf8 $ contentFileContent
