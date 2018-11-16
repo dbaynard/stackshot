e4897093e3aaa49fb69ef4fd649843ae2c5cd8f3
David Baynard <davidbaynard@gmail.com>
2018-11-16 15:25:57 +0100

    Add executable

--- stackshot/app/Main.hs
+++ stackshot/app/Main.hs
@@ -1,5 +1,32 @@
+{-# LANGUAGE
+    PackageImports
+  , DataKinds
+  , DeriveGeneric
+  , DerivingStrategies
+  , FlexibleInstances
+  , OverloadedStrings
+  , RecordWildCards
+  , StandaloneDeriving
+  , TypeInType
+  , TypeOperators
+  #-}
+
 module Main where
+
+import "optparse-generic" Options.Generic
+
+import Stackshot (runUpToDate)
+
+data Options w = Options
+    { input :: w ::: FilePath <?> "Input snapshot.yaml (avoid github packages)"
+    , output :: w ::: FilePath <?> "Output package list"
+    } deriving (Generic)
+
+instance ParseRecord (Options Wrapped)
+deriving instance Show (Options Unwrapped)
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+    Options{..} <- unwrapRecord "List updates to packages in resolver"
+    runUpToDate input output
+
